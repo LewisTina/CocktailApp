e12793dd1a157d45057e44fa44972ca6e137f53e
LewisTina <aboutou23@outlook.fr>
2025-02-24 13:09:17 +0100

    clean console

--- src/screens/SearchResultScreen.js
+++ src/screens/SearchResultScreen.js
@@ -7,14 +7,12 @@ const SearchResultScreen = ({ navigation, route }) => {
     const [ items, setItems ] = useState('');
     const [ isLoading, setIsLading ] = useState(true);
     const { scope, value } = route.params;
-    console.log(route.params)
 
     const retrieveItems = async () => {
         try {
         const API_URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?${scope}=${value}`;
         const response = await axios.get(API_URL);
         const data = response.data;
-        console.log(data)
         setItems(data.drinks);
         } catch (e) {
           console.log("Erreur lors de l'appel à l'API : " + e);
